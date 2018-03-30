fb9c555edf48e659b13f96cdfd9828b73b41cb5e
Tom Dugan <thomas@aadwebb.com>
2018-03-29 19:41:48 -0500

    added xhr

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log();
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+    myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+    myRequest.open("GET", animals.json);
+    myRequest.send();
+}
+
+function executeThisCodeIfXHRFails () {
+    console.log("something broke")
+}
+
+function executeThisCodeAfterFileLoaded () {
+    const data = JSON.parse(this.responseText);
+}
+
+startApplication();
