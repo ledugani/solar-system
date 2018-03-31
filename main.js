a024db250ad1c5c2a7e9ad6548bdd67e4275ac24
Tom Dugan <thomas@aadwebb.com>
2018-03-31 11:55:29 -0500

    wip - have all info for modal box, just need to be able to select a specific one

--- main.js
+++ main.js
@@ -12,11 +12,11 @@ const buildDomString = fancyArray => {
         domString += `<div class="name">${planet.name}</div>`;
         domString += `<div class="middle">`;
         if (`${planet.name}` === `Jupiter`) {
-            domString += `<img src="${planet.imageUrl}" id='jupiter' class="image">`;
+            domString += `<img src="${planet.imageUrl}" id='jupiter planet' class="image">`;
         } else if (`${planet.name}` === `Saturn`) {
-            domString += `<img src="${planet.imageUrl}" id='saturn' class="image">`;
+            domString += `<img src="${planet.imageUrl}" id='saturn planet' class="image">`;
         } else {
-            domString +=    `<img src="${planet.imageUrl}" class="image">`;
+            domString +=    `<img src="${planet.imageUrl}" id='planet' class="image">`;
         }
         domString += `</div> </div>`;
     });
@@ -44,42 +44,57 @@ const startApplication = () => {
 
 startApplication();
 
-// domString for big cards
+/*---------------------------------- BIG CARDS ----------------------------------*/
 
-const buildBigCard = fancyArray => {
-    let newDomString = "";
-    fancyArray.forEach((planet) => {
-        newDomString += `<div id="myModal" class="modal">`;
-        newDomString +=     `<div class="modal-content">`;
-        newDomString +=         `<span class="close">&times;</span>`;
-        newDomString +=         `<h1>${planet.name}</h1>`;
-        newDomString +=         `<img src="${planet.imageUrl}" class="image">`;
-        if (`${planet.isGasPlanet}`) {
-            newDomString +=         `<h3>Gas Planet</h3>`;
-        } else {
-            newDomString +=         `<h3>Terrestrial Planet</h3>`;
-        } 
-        newDomString +=         `<h3>Number of Moons: ${planet.numberOfMoons}</h3>`;
-        newDomString +=         `<h3>Name of Largest Moon: ${planet.nameOfLargestMoon}</h3>`;
-        newDomString +=         `<p>${planet.description}</p>`;
-        newDomString += `</div> </div>`;
-    });
-    printToDom(domString, 'card-holder');
-}
+const modal = document.getElementById('myModal');
 
 // XHR for Big Cards
 
-function exeThisIfLoads () {
-    const newData = JSON.parse(this.responseText);
-    buildBigCard(newData.planets);
-}
-
 const xhrForBigCard = () => {
     let myNewRequest = new XMLHttpRequest();
-    myNewRequest.addEventListener("load", exeThisIfLoads);
+    myNewRequest.addEventListener("load", addPlanetEventListeners);
     myNewRequest.addEventListener("error", executeThisCodeIfXHRFails);
     myNewRequest.open("GET", "planets.json");
     myNewRequest.send();
 }
+
+// domString for big cards
+
+const buildBigCard = fancierArray => {
+    let newDomString = "";
+    fancierArray.forEach((planet) => {
+        newDomString += `<div class="modal-content">`;
+        newDomString +=     `<span class="close">&times;</span>`;
+        newDomString +=     `<h1>${planet.name}</h1>`;
+        newDomString +=     `<img src="${planet.imageUrl}" class="image">`;
+        if (`${planet.isGasPlanet}`) {
+            newDomString +=     `<h3>Gas Planet</h3>`;
+        } else {
+            newDomString +=     `<h3>Terrestrial Planet</h3>`;
+        } 
+        newDomString +=     `<h3>Number of Moons: ${planet.numberOfMoons}</h3>`;
+        newDomString +=     `<h3>Name of Largest Moon: ${planet.nameOfLargestMoon}</h3>`;
+        newDomString +=     `<p>${planet.description}</p>`;
+        newDomString += `</div>`;
+    });
+    modal.style.display = "block";
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+}
+
+const displayModalBox = () => {
+    const newData = JSON.parse(this.responseText);
+    buildBigCard(newData.planets);
+}
+
+// Event Listener for each card
+
+const addPlanetEventListeners = () => {
+    const planetButtons = document.getElementsByClassName('image');
+    for(let i=0; i < planetButtons.length; i++) {
+        planetButtons[i].addEventListener('click', displayModalBox);
+    }
+};
 
 xhrForBigCard();
